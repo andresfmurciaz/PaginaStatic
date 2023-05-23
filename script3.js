2d880834e151d7ee8634bf33abb43c3fab7ed330
andresf.murciaz <andresf.murciaz@gmail.com>
2023-05-23 15:16:12 -0500

    boton conclu

---
+++ script3.js
@@ -0,0 +1,9 @@
+document.getElementById("toggleBtnnn").addEventListener("click", function() {
+    var myList = document.getElementById("myLista2");
+    if (myList.style.display === "none") {
+      myList.style.display = "block";
+    } else {
+      myList.style.display = "none";
+    }
+  });
+  
